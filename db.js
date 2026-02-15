1d0f003378ffe7a93b7f9a382c133e435526673c
Peter Uhunoma <dariusadler2026@gmail.com>
2026-02-15 11:54:54 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,15 +2,16 @@ const { Pool } = require("pg");
 require('dotenv').config();
 
 const pool = new Pool({
-    host: "localhost",
-    user: "postgres",
-    password: "",
-    database: "aperture",
-    port: 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+    user: 'postgres',
+    host: 'db.ambzrigctcmrshdvnvlo.supabase.co',
+    database: 'postgres',
+    password: '#freezeDarius1',
+    port: 6543,
+    ssl: {
+      rejectUnauthorized: false
+    },
+    connectionTimeoutMillis: 10000, // 10 second timeout
+    options: '-c search_path=public'
+  });
  
-
 module.exports = pool;
