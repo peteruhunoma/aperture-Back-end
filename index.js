2d058f8f9ab1d0630f124c89fd44385ee0070b16
Peter Uhunoma <dariusadler2026@gmail.com>
2026-03-10 10:10:44 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const app = express();
 
                                                   
 app.use(cors(
-  {origin: 'http://localhost:5173', 
+  {origin: 'https://aperture-two.vercel.app', 
 credentials: true}));
 app.use(cookieParser());
 app.use(express.json()); 
